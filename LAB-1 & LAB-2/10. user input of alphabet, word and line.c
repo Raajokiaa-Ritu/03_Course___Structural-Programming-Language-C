55f5dc3429500abf1567edffd203b1bf27116cc4
Khandaker Jannatul Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2024-04-26 10:50:12 +0600

    Update and rename 10. user input of line.c to 10. user input of alphabet, word and line.c

--- LAB-1 & LAB-2/10. user input of alphabet, word and line.c
+++ LAB-1 & LAB-2/10. user input of alphabet, word and line.c
@@ -2,12 +2,28 @@
 
 int main()
 {
+    /// user input of an alphabet---
+    char ch;
+    printf("Enter an alphabet : ");
+    scanf("%c", &ch);
+    printf("you have typed: %c \n\n", ch);
 
+
+
+    ///user input of a word---
+    char word[50];
+    printf("Enter a word : ");
+    getchar();
+    scanf("%s", &word);
+    printf("the word you typed : %s \n\n", word);
+
+
+    getchar();
     ///how to take user input a complete line??
     char name1[100];
     printf(" Enter a line : ");
     scanf("%[^\n]s", &name1); ///Look at the scanf()!!
-    printf(" Text : %s", name1);
+    printf(" Text : %s\n\n", name1);
 
     /// what is '%[^n]s' ??
     /*
@@ -21,5 +37,6 @@ The &name1 passes the address of the name1
 array to scanf for storing the input.
     */
 
-
+getch();
 }
+
