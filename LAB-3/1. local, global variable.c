7dda8d6e7ad29e316828c539d2738eb7ebca7fa8
Khandaker Jannatul Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2024-04-28 11:49:05 +0600

    Update and rename 1. local, global, static variable.c to 1. local, global variable.c

--- LAB-3/1. local, global variable.c
+++ LAB-3/1. local, global variable.c
@@ -9,10 +9,6 @@ int number1 = 111;
 float ff;
 double d;
 
-static double wgt = 4.5;
-static char ch = 'a';
-static int id;
-
 int main()
 {
     int number1 = 101; //local variable
@@ -22,13 +18,5 @@ int main()
     /// local variable will get the priority first
     printf("number 1 is = %d\n", number1);
     printf("number 2 is = %d\n", number2);
-
-
-    ///-----static variable--------
-    static int sum1,sum2;
-    static float sum3, sum4;
-    scanf("%d %d", &sum1, &id);
-    printf("%d %d\n", sum1, id);
-
 
 }
